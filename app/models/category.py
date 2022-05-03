44b456ea1063eb008a367e6004451739fca6dbd7
WayneMusungu <waynemuyera17@gmail.com>
2022-05-03 09:47:25 +0300

    Add changes

--- app/models/category.py
+++ app/models/category.py
@@ -3,10 +3,12 @@ class Category:
     '''
     Class that instantiates objects of the news categories objects of the news sources
     '''
-    def __init__(self,author,description,time,url,image,title):
+    
+    def __init__(self, author, title, description, url, urlToImage, publishedAt, content):
         self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
         self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
